7580ba59997fde8e625218ed5ab4cf0118ea3984
Mischback <dev@mischback.de>
2022-03-29 16:42:49 +0200

    Fetch main component from template app

---
+++ main/main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Mischback
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileType: SOURCE
+ *
+ * This is the applications main file, providing the required main entry point
+ * of the application.
+ */
+
+#include <stdio.h>
+
+void app_main(void) {
+    // here be magic!
+}
