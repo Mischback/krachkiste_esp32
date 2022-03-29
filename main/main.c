ae7be07e2626176d080768c1b3645fc0c22b4355
Mischback <dev@mischback.de>
2022-03-29 17:07:34 +0200

    Provide logging for app_main()

--- main/main.c
+++ main/main.c
@@ -7,8 +7,21 @@
  * of the application.
  */
 
-#include <stdio.h>
+/* This is ESP-IDF's logging library.
+ * - ESP_LOGE(TAG, "Error");
+ * - ESP_LOGW(TAG, "Warning");
+ * - ESP_LOGI(TAG, "Info");
+ * - ESP_LOGD(TAG, "Debug");
+ * - ESP_LOGV(TAG, "Verbose");
+*/
+#include <esp_log.h>
+
+// label for the file (or module)-specific logger
+static const char* TAG = "Krachkiste.main";
 
 void app_main(void) {
-    // here be magic!
+    // set log-level of our own code to DEBUG (sdkconfig.defaults sets the
+    // default log-level to INFO)
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+    ESP_LOGD(TAG, "Entering app_main()");
 }
