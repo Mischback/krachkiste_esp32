70bce9919895be184f15d970857263a00d429288
Mischback <dev@mischback.de>
2022-03-30 19:27:45 +0200

    Fetch ESP-IDF example wifi/scan
    This is the code from
    https://github.com/espressif/esp-idf/blob/master/examples/wifi/scan/main/scan.c
    transformed to be placed in a component and then called from the applicaion's
    app_main().
    
    Nothing really fancy here, but a first shot at the desired WiFi functionality.

--- src/lib/wifi_management/wifi_management.c
+++ src/lib/wifi_management/wifi_management.c
@@ -6,6 +6,11 @@
  * Provide a basic management of the WiFi capabilities of the MCU.
  */
 
+// This is the components header file (forward declaration)
+#include "wifi_management.h"
+
+#include <string.h>  // porting example code
+
 /* This is ESP-IDF's logging library.
  * - ESP_LOGE(TAG, "Error");
  * - ESP_LOGW(TAG, "Warning");
@@ -15,8 +20,12 @@
 */
 #include "esp_log.h"
 
-// This is the components header file (forward declaration)
-#include "wifi_management.h"
+#include "esp_event.h"  // porting example code
+#include "esp_wifi.h"  // porting example code
+#include "nvs_flash.h"  // porting example code
+
+
+#define DEFAULT_SCAN_LIST_SIZE 5
 
 
 /**
@@ -32,4 +41,46 @@ void initialize_wifi(void) {
     // default log-level to INFO)
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
     ESP_LOGD(TAG, "Entering initializewifi()");
+
+
+    // ported example code
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    uint16_t max_number = DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_scan_start(NULL, true);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&max_number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
+    for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
+        ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
+        ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
+        // print_auth_mode(ap_info[i].authmode);
+        if (ap_info[i].authmode != WIFI_AUTH_WEP) {
+            // print_cipher_type(
+            //     ap_info[i].pairwise_cipher,
+            //     ap_info[i].group_cipher);
+        }
+        ESP_LOGI(TAG, "Channel \t\t%d\n", ap_info[i].primary);
+    }
 }
