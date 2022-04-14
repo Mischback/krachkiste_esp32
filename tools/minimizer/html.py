f938f1e1b51e5922bf78808de7fe81ffe9336fe1
Mischback <dev@mischback.de>
2022-04-14 14:06:43 +0200

    Improve minimizer/html.py

--- tools/minimizer/html.py
+++ tools/minimizer/html.py
@@ -1,3 +1,13 @@
+# SPDX-FileCopyrightText: 2022 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+"""Minify HTML source code with ``minifiy-html``.
+
+The script is basically just a tiny wrapper around ``minify-html`` and sets the
+desired configuration values. These are hard-coded, to create coherent results
+for all files during every run.
+"""
+
 # Python imports
 import sys
 
@@ -5,17 +15,28 @@ import sys
 import minify_html
 
 if __name__ == "__main__":
-    # path of this script
-    # print("Running minimizer/html ({})".format(sys.argv[0]))
-
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    # get parameters from ``argv``
+    try:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+    except IndexError:
+        print("Please specify an INPUT / OUTPUT file!")
+        sys.exit(1)
 
     print("Minimizing HTML source {}".format(input_file))
 
     # open input_file, read its content
-    with open(input_file) as f_in:
-        source = f_in.read()
+    try:
+        with open(input_file) as f_in:
+            source = f_in.read()
+    except FileNotFoundError:
+        print("Could not open INPUT '{}' (file not found)!".format(input_file))
+        sys.exit(1)
+    except PermissionError:
+        print(
+            "Could not read INPUT '{}' (missing permission)!".format(input_file)
+        )
+        sys.exit(1)
 
     # run through minify_html
     # all configuration options are specified explicitly!
@@ -34,7 +55,15 @@ if __name__ == "__main__":
     )
 
     # write to output_file
-    with open(output_file, "w") as f_out:
-        f_out.write(result)
+    try:
+        with open(output_file, "w") as f_out:
+            f_out.write(result)
+    except PermissionError:
+        print(
+            "Could not write to OUTPUT '{}' (missing permission)".format(
+                output_file
+            )
+        )
+        sys.exit(1)
 
     sys.exit(0)
