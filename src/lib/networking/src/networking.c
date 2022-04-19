09c10212be523a2c693a6be7c287f399426ebab9
Mischback <dev@mischback.de>
2022-04-19 22:15:39 +0200

    Get rid of wifi_scan_for_networks()

--- src/lib/networking/src/networking.c
+++ src/lib/networking/src/networking.c
@@ -61,13 +61,6 @@
 
 
 /**
- * Determine the maximum number of WiFi networks to be included in a scan.
- *
- * This is used by ::wifi_scan_for_networks.
- */
-#define DEFAULT_SCAN_LIST_SIZE 5
-
-/**
  * The maximum length of the ``char`` array to store SSID.
  *
  * IEEE 802.11 says, that the maximum length of an SSID is 32, so this is set
@@ -106,58 +99,12 @@ static const char* TAG = "krachkiste.networking";
 
 
 /* ***** PROTOTYPES ******************************************************** */
-static void wifi_scan_for_networks(void);
 static esp_err_t networking_get_wifi_credentials(
     char* wifi_ssid,
     char* wifi_password);
 
 
 /* ***** FUNCTIONS ********************************************************* */
-
-/**
- * Scan for all available wifi networks.
- *
- * This was actually the first example code, that was imported to get started
- * with this component.
- *
- * As of now, the function is **not in use**.
- *
- * The code is directly fetched from
- * https://github.com/espressif/esp-idf/tree/master/examples/wifi/scan
- */
-static void wifi_scan_for_networks(void) {
-    // ported example code
-    ESP_ERROR_CHECK(esp_netif_init());
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-    assert(sta_netif);
-
-    // initialize the wifi configuration with known defaults (of ESP-IDF)
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    uint16_t max_number = DEFAULT_SCAN_LIST_SIZE;
-    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
-    uint16_t ap_count = 0;
-    memset(ap_info, 0, sizeof(ap_info));
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    esp_wifi_scan_start(NULL, true);
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&max_number, ap_info));
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-    ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
-    for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
-        ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
-        ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
-        // print_auth_mode(ap_info[i].authmode);
-        if (ap_info[i].authmode != WIFI_AUTH_WEP) {
-            // print_cipher_type(
-            //     ap_info[i].pairwise_cipher,
-            //     ap_info[i].group_cipher);
-        }
-        ESP_LOGI(TAG, "Channel \t\t%d\n", ap_info[i].primary);
-    }
-}
 
 /**
  * Retrieve credentials for a WiFi network from the NVS.
