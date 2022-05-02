544ffa9c54df89ec2dc680d0f0e7286a10ba377f
Mischback <dev@mischback.de>
2022-05-02 09:30:35 +0200

    Move type definitions to networking.c

--- src/lib/networking/src/networking.c
+++ src/lib/networking/src/networking.c
@@ -116,6 +116,68 @@
 
 /* ***** TYPES ************************************************************* */
 
+/**
+ * This is the list of accepted notifications.
+ */
+typedef enum {
+    NETWORKING_NOTIFICATION_BASE,
+    NETWORKING_NOTIFICATION_CMD_WIFI_START,
+} networking_notification;
+
+/**
+ * Specify the actual connection medium.
+ *
+ * This might be a wired connection (``NETWORKING_MEDIUM_ETHERNET``) or a
+ * wireless connection (``NETWORKING_MEDIUM_WIRELESS``) when the component is
+ * actually up and running.
+ *
+ * This is actually tracked in the component's ::state.
+ */
+typedef enum {
+    NETWORKING_MEDIUM_UNSPECIFIED,
+    NETWORKING_MEDIUM_ETHERNET,
+    NETWORKING_MEDIUM_WIRELESS,
+} networking_medium;
+
+/**
+ * Specify the mode of the wireless connection.
+ *
+ * This is only applicable for ``NETWORKING_MEDIUM_WIRELESS`` and will be set
+ * to ``NETWORKING_MODE_NOT_APPLICABLE`` on initialization or if the medium
+ * is set to ``NETWORKING_MEDIUM_ETHERNET``.
+ */
+typedef enum {
+    NETWORKING_MODE_NOT_APPLICABLE,
+    NETWORKING_MODE_WIFI_AP,
+    NETWORKING_MODE_WIFI_STA,
+} networking_mode;
+
+/**
+ * Specify the actual status of the connection.
+ *
+ * The connection status must be evaluated in the context of its ``medium`` -
+ * and in case of a wireless connection - its ``mode``.
+ */
+typedef enum {
+    NETWORKING_STATUS_DOWN,
+    NETWORKING_STATUS_READY,
+    NETWORKING_STATUS_UP,
+} networking_status;
+
+/**
+ * A component-specific struct to keep track of the internal state.
+ */
+struct networking_state {
+    networking_medium   medium;
+    networking_mode     mode;
+    networking_status   status;
+    esp_netif_t         *interface;
+    TaskHandle_t        task;
+    esp_event_handler_t ip_event_handler;
+    esp_event_handler_t wifi_event_handler;
+};
+
+
 /* ***** VARIABLES ********************************************************* */
 
 /**
