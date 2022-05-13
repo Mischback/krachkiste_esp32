d197509189e5d2dfbaab4ef13b66df692894dc7f
Mischback <dev@mischback.de>
2022-05-13 16:24:21 +0200

    Add mnet32_nvs

---
+++ src/lib/embedded_networking_esp32/src/mnet32_nvs.c
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2022 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+/**
+ * Provide access to the non-volatile storage for the ``mnet32`` component.
+ *
+ * @file   mnet32_nvs.c
+ * @author Mischback
+ * @bug    Bugs are tracked with the
+ *         [issue tracker](https://github.com/Mischback/krachkiste_esp32/issues)
+ *         at GitHub.
+ */
+
+/* ***** INCLUDES ********************************************************** */
+
+/* This file's header. */
+#include "mnet32_nvs.h"
