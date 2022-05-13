27b2d760f3fdfd01e481d1fa049cdff6734edec0
Mischback <dev@mischback.de>
2022-05-13 09:56:44 +0200

    Create networking_state module

---
+++ src/lib/networking/src/networking_state.c
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2022 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+/**
+ * Functions to manage the internal state of the ``networking`` component.
+ *
+ * @file   networking_state.c
+ * @author Mischback
+ * @bug    Bugs are tracked with the
+ *         [issue tracker](https://github.com/Mischback/krachkiste_esp32/issues)
+ *         at GitHub.
+ */
+
+/* ***** INCLUDES ********************************************************** */
+
+/* This file's header. */
+#include "networking_state.h"
