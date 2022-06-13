d265bc0f011830b1afac748faee711d51f3bd98d
Mischback <dev@mischback.de>
2022-06-13 12:43:28 +0200

    Add documentation to mnet32_nvs

--- src/lib/embedded_networking_esp32/src/mnet32_nvs.h
+++ src/lib/embedded_networking_esp32/src/mnet32_nvs.h
@@ -14,8 +14,36 @@
 #include "nvs_flash.h"
 
 
+/**
+ * Get a handle to the component's non-volatile storage.
+ *
+ * Opens a handle to ::MNET32_NVS_NAMESPACE of the given ``mode`` and stores
+ * the actual handle in ``handle``.
+ *
+ * Please note that the calling function will have to allocate memory for
+ * ``handle``.
+ *
+ * @param mode   The mode to open the handle in.
+ * @param handle The actual handle.
+ * @return esp_err_t ``ESP_OK`` if the handle could be successfully opened.
+ */
 esp_err_t mnet32_nvs_get_handle(nvs_open_mode_t mode, nvs_handle_t* handle);
 
+/**
+ * Get a string value from the non-volatile storage as specified by ``key``.
+ *
+ * The ``handle`` must be established before calling this funcion (see
+ * ::mnet32_nvs_get_handle ). The memory to store the retrieved value must be
+ * allocated before calling this function.
+ *
+ * @param handle       The handle to use.
+ * @param key          The key to retrieve.
+ * @param ret_buffer   A pointer to the allocated memory to store the value.
+ * @param max_buf_size Maximum length of the value to be retrieved. This should
+ *                     match the allocated memory for the value.
+ * @return esp_err_t   ``ESP_OK`` if the value could be retrieved, some
+ *                     NVS-specific error code otherwise.
+ */
 esp_err_t mnet32_nvs_get_string(nvs_handle_t handle,
                                 const char* key,
                                 char* ret_buffer,
