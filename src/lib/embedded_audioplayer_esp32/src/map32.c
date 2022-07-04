8f442c947389c97408ba941136972a472e1244aa
Mischback <dev@mischback.de>
2022-07-04 09:12:51 +0200

    Create component for map32 (#21)

---
+++ src/lib/embedded_audioplayer_esp32/src/map32.c
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2022 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+/**
+ * Provide the actual audio player.
+ *
+ * @file   map32.c
+ * @author Mischback
+ * @bug    Bugs are tracked with the
+ *         [issue tracker](https://github.com/Mischback/krachkiste_esp32/issues)
+ *         at GitHub.
+ */
+
+/* ***** INCLUDES ********************************************************** */
+
+/* This file's header. */
+#include "map32/map32.h"
+
+/* ***** DEFINES *********************************************************** */
+
+/* ***** TYPES ************************************************************* */
+
+/* ***** VARIABLES ********************************************************* */
+
+/* ***** PROTOTYPES ******************************************************** */
+
+/* ***** FUNCTIONS ********************************************************* */
