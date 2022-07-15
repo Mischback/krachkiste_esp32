4253bdb86fa94d1a6c0909d3e20447994342bb2e
Mischback <dev@mischback.de>
2022-07-15 14:31:08 +0200

    Add initialization of common audio elements

--- src/lib/embedded_audioplayer_esp32/src/map32.c
+++ src/lib/embedded_audioplayer_esp32/src/map32.c
@@ -17,6 +17,8 @@
 /* This file's header. */
 #include "map32/map32.h"
 
+#include "audio_pipeline.h"
+
 /* This is ESP-IDF's error handling library.
  * - defines ``esp_err_t``
  * - defines common return values (``ESP_OK``, ``ESP_FAIL``)
@@ -33,6 +35,8 @@
  * - provided by ESP-IDF's component ``log``
  */
 #include "esp_log.h"
+#include "i2s_stream.h"
+#include "mp3_decoder.h"
 
 /* ***** DEFINES *********************************************************** */
 
@@ -47,6 +51,10 @@
  * [its API documentation](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html#how-to-use-this-library).
  */
 static const char* TAG = "map32";
+
+static audio_pipeline_handle_t map32_pipeline = NULL;
+static audio_element_handle_t map32_i2s_writer = NULL;
+static audio_element_handle_t map32_decoder = NULL;
 
 /* ***** PROTOTYPES ******************************************************** */
 
@@ -74,12 +82,70 @@ static esp_err_t map32_init(void) {
 
     ESP_LOGV(TAG, "map32_init()");
 
+    ESP_LOGV(TAG, "Building default pipeline configuration...");
+    audio_pipeline_cfg_t pipeline_config = DEFAULT_AUDIO_PIPELINE_CONFIG();
+    map32_pipeline = audio_pipeline_init(&pipeline_config);
+    if (map32_pipeline == NULL) {
+        ESP_LOGE(TAG, "Could not initialize audio pipeline!");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGV(TAG, "Setting up common audio elements...");
+
+    ESP_LOGV(TAG, "Setup of I2S stream writer...");
+    i2s_stream_cfg_t i2s_config = I2S_STREAM_CFG_DEFAULT();
+    i2s_config.type = AUDIO_STREAM_WRITER;
+    // TODO(mischback) This is probably the place to set the actual pins of
+    //                 I2S, but there is no example actually doing this. Needs
+    //                 more research!
+    //                 https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/i2s.html
+    //                 i2s_config.i2s_config.gpio_cfg.mclk = (?!?)
+    // TODO(mischback) This may be guarded with an #ifdef to make this
+    //                 compatible with ESP-ADF's logic
+    map32_i2s_writer = i2s_stream_init(&i2s_config);
+    if (map32_i2s_writer == NULL) {
+        ESP_LOGE(TAG, "Could not initialize i2s_writer!");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGV(TAG, "Setup of MP3 decoder...");
+    mp3_decoder_cfg_t mp3_config = DEFAULT_MP3_DECODER_CONFIG();
+    map32_decoder = mp3_decoder_init(&mp3_config);
+    if (map32_decoder == NULL) {
+        ESP_LOGE(TAG, "Could not initialize decoder!");
+        return ESP_FAIL;
+    }
+
     return ESP_OK;
 }
 
 // TODO(mischback) Add documentation!
 static esp_err_t map32_deinit(void) {
     ESP_LOGV(TAG, "map32_deinit()");
+
+    esp_err_t esp_ret;
+
+    ESP_LOGV(TAG, "Deinitializing audio pipeline...");
+    esp_ret = audio_pipeline_deinit(map32_pipeline);
+    if (esp_ret != ESP_OK) {
+        ESP_LOGE(TAG, "Could not deinitialize audio pipeline.");
+        ESP_LOGD(TAG, "Calling free()");
+        free(map32_pipeline);
+    }
+
+    esp_ret = audio_element_deinit(map32_i2s_writer);
+    if (esp_ret != ESP_OK) {
+        ESP_LOGE(TAG, "Could not deinitialize i2s_writer.");
+        ESP_LOGD(TAG, "Calling free()");
+        free(map32_i2s_writer);
+    }
+
+    esp_ret = audio_element_deinit(map32_decoder);
+    if (esp_ret != ESP_OK) {
+        ESP_LOGE(TAG, "Could not deinitialize decoder.");
+        ESP_LOGD(TAG, "Calling free()");
+        free(map32_decoder);
+    }
 
     return ESP_OK;
 }
